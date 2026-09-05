07fa430c81fcd6b3de0b2cbcf27493b38fdd4dd6
Kavan Dadkhah <kavandadkhah@gmail.com>
2026-09-05 11:09:44 +0330

    chore: add initial Kadad Class welcome view

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fa" dir="rtl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>کاداد کلاس</title>
+</head>
+<body>
+    <main>
+        <h1>کاداد کلاس</h1>
+        <p>پلتفرم کلاس آنلاین کاداد در حال ساخت است.</p>
+    </main>
+</body>
+</html>
