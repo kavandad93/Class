248ed4b23298b94d4747be3455f3d6aa92dfa7a1
Kavan Dadkhah <kavandadkhah@gmail.com>
2026-09-06 13:34:54 +0330

    Add shared home page footer

---
+++ home/_footer.php
@@ -0,0 +1,5 @@
+<footer>
+<div class="footer-links"><a href="/home">خانه</a><a href="/home/features.php">امکانات</a><a href="/home/about.php">درباره</a><a href="/home/contact.php">تماس</a></div>
+© کاداد کلاس
+</footer>
+</body></html>
