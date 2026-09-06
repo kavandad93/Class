486f7193ed9517541fe9e0d0f428599ea7d6cdbd
Kavan Dadkhah <kavandadkhah@gmail.com>
2026-09-06 12:55:33 +0330

    Load live permission polling

--- join/ads_prepend.php
+++ join/ads_prepend.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 if (str_starts_with((string)($_SERVER['REQUEST_URI'] ?? ''), '/join/') && (($_GET['enter'] ?? '') === '1')) {
     ob_start(static function (string $html): string {
         if (stripos($html, 'id="live"') === false || stripos($html, '</body>') === false) return $html;
-        $scripts = '<script src="/ads/player.js"></script><script src="/join/class_features.js"></script>';
+        $scripts = '<script src="/ads/player.js"></script><script src="/join/class_features.js"></script><script src="/join/permissions_live.js"></script>';
         return preg_replace('~</body>~i', $scripts . '</body>', $html, 1) ?? $html;
     });
 }
