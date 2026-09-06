6504a16707cd7939698ca14e3d0f9387f1c2e4a4
Kavan Dadkhah <kavandadkhah@gmail.com>
2026-09-06 12:54:02 +0330

    Load live class management features

--- join/ads_prepend.php
+++ join/ads_prepend.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 if (str_starts_with((string)($_SERVER['REQUEST_URI'] ?? ''), '/join/') && (($_GET['enter'] ?? '') === '1')) {
     ob_start(static function (string $html): string {
         if (stripos($html, 'id="live"') === false || stripos($html, '</body>') === false) return $html;
-        $script = '<script src="/ads/player.js"></script>';
-        return preg_replace('~</body>~i', $script . '</body>', $html, 1) ?? $html;
+        $scripts = '<script src="/ads/player.js"></script><script src="/join/class_features.js"></script>';
+        return preg_replace('~</body>~i', $scripts . '</body>', $html, 1) ?? $html;
     });
 }
