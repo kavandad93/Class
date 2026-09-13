76e512a5024ab9ca917655b8560075b1486551b8
Kavan Dadkhah <kavandadkhah@gmail.com>
2026-09-13 12:56:42 +0330

    Ensure class audio controls load fresh

--- join/ads_prepend.php
+++ join/ads_prepend.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 if (str_starts_with((string)($_SERVER['REQUEST_URI'] ?? ''), '/join/') && (($_GET['enter'] ?? '') === '1')) {
     ob_start(static function (string $html): string {
         if (stripos($html, '</body>') === false) return $html;
-        $scripts = '<script src="/ads/player.js"></script><script src="/join/class_features.js"></script><script src="/join/permissions_live.js"></script><script src="/join/audio_gain.js"></script>';
+        $scripts = '<script src="/ads/player.js?v=20260913"></script><script src="/join/class_features.js?v=20260913"></script><script src="/join/permissions_live.js?v=20260913"></script><script src="/join/audio_gain.js?v=20260913"></script>';
         return preg_replace('~</body>~i', $scripts . '</body>', $html, 1) ?? $html;
     });
 }
